4e74c22287deb175221298482efd9b51681d92c1
Xoy <pinrada2020@gmail.com>
2025-04-22 23:28:16 +0700

    Initial Commit

---
+++ todo-list/todo_listApp.swift
@@ -0,0 +1,17 @@
+//
+//  todo_listApp.swift
+//  todo-list
+//
+//  Created by Pstechno01 on 22/4/2568 BE.
+//
+
+import SwiftUI
+
+@main
+struct todo_listApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
